452ba595845b877e29758a2a9c9f683b24796c93
ArcherHeffern <heffernarcher@gmail.com>
2025-12-07 05:26:33 -0500

    Added some more typing to crypto service

--- crypto.py
+++ crypto.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
-from typing import NewType, Optional
+from typing import NamedTuple, NewType, Optional
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.exceptions import InvalidSignature
@@ -13,30 +14,58 @@ Signature = NewType("Signature", bytes)
 CipherText = NewType("CipherText", bytes)
 
 
-def create_key_pair_for_encryption() -> tuple[
-    EncryptionPublicKey, EncryptionPrivateKey
-]:
+class EncryptionKeys(NamedTuple):
+    pub: EncryptionPublicKey
+    priv: EncryptionPrivateKey
+
+
+class SigningKeys(NamedTuple):
+    pub: SigningPublicKey
+    priv: SigningPrivateKey
+
+
+class PublicKeys(NamedTuple):
+    encryption_key: EncryptionPublicKey
+    signing_key: SigningPublicKey
+
+
+@dataclass
+class Person:
+    encryption_keys: EncryptionKeys
+    signing_keys: SigningKeys
+
+    @staticmethod
+    def create() -> Person:
+        return Person(create_key_pair_for_encryption(), create_key_pair_for_signing())
+
+    def get_public_keys(self) -> PublicKeys:
+        return PublicKeys(self.encryption_keys.pub, self.signing_keys.pub)
+
+
+def create_key_pair_for_encryption() -> EncryptionKeys:
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
-    return EncryptionPublicKey(private_key.public_key()), EncryptionPrivateKey(
-        private_key
+    return EncryptionKeys(
+        EncryptionPublicKey(private_key.public_key()), EncryptionPrivateKey(private_key)
     )
 
 
-def create_key_pair_for_signing() -> tuple[SigningPublicKey, SigningPrivateKey]:
+def create_key_pair_for_signing() -> SigningKeys:
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
-    return SigningPublicKey(private_key.public_key()), SigningPrivateKey(private_key)
+    return SigningKeys(
+        SigningPublicKey(private_key.public_key()), SigningPrivateKey(private_key)
+    )
 
 
-def sign(message: str, private_key: SigningPrivateKey) -> Signature:
+def sign(message: bytes, private_key: SigningPrivateKey) -> Signature:
     return Signature(
         private_key.sign(
-            message.encode(),
+            message,
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
             ),
@@ -45,11 +74,11 @@ def sign(message: str, private_key: SigningPrivateKey) -> Signature:
     )
 
 
-def verify(signature: Signature, message: str, public_key: SigningPublicKey) -> bool:
+def verify(signature: Signature, message: bytes, public_key: SigningPublicKey) -> bool:
     try:
         public_key.verify(
             signature,
-            message.encode(),
+            message,
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
             ),
@@ -60,10 +89,10 @@ def verify(signature: Signature, message: str, public_key: SigningPublicKey) -> 
         return False
 
 
-def encrypt(message: str, public_key: EncryptionPublicKey) -> CipherText:
+def encrypt(message: bytes, public_key: EncryptionPublicKey) -> CipherText:
     return CipherText(
         public_key.encrypt(
-            (message * 2).encode(),
+            (message * 2),
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
                 algorithm=hashes.SHA256(),
@@ -86,7 +115,7 @@ def decrypt(ciphertext: CipherText, private_key: EncryptionPrivateKey) -> Option
         return None
 
     first_half = msg[0 : len(msg) // 2]
-    second_half = msg[len(msg) // 2:]
+    second_half = msg[len(msg) // 2 :]
     if first_half != second_half:
         return None
     return first_half.decode()
